eec8cb7e52832a18976b4b9c01f7dc1ed207247d
hpaguio <hendrickpaguio@gmail.com>
2025-03-21 09:43:29 +0800

    [urls.py] - Added login and logout pages

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
     path('recipe/<int:id>/', views.recipe_detail, name='recipe_detail'),
-    path('recipe/list', views.recipe_list, name="recipe_list")
+    path('recipe/list', views.recipe_list, name="recipe_list"),
+    path('login/', auth_views.LoginView.as_view(template_name='ledger/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
 
 app_name = "ledger"
