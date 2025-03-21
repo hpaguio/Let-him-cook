e1fc305185edf1fae91d7ddf641bbdea5a5336fc
hpaguio <hendrickpaguio@gmail.com>
2025-03-21 09:33:38 +0800

    [models.py] - Added the Profile model and added an author field in Recipe

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.urls import reverse
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
@@ -9,6 +10,9 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipes')
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
@@ -20,3 +24,11 @@ class RecipeIngredient(models.Model):
 
     def __str__(self):
         return f"{self.quantity} of {self.ingredient.name} for {self.recipe.name}"
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.user.username
